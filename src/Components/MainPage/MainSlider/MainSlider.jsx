ac3f01d6d45ea1341155883cda0ca5492d330282
Vladislav Stepaniuk <p.s.vlad2014@gmail.com>
2019-12-01 17:12:48 +0200

    MainPAge add circles

--- src/Components/MainPage/MainSlider/MainSlider.jsx
+++ src/Components/MainPage/MainSlider/MainSlider.jsx
@@ -7,8 +7,8 @@ import {NavLink} from 'react-router-dom';
 const MainSlider = () =>{
 	return(
 		<div className='MainSlider' id='mainslider'>
-		<div>
-			<div>
+			<div className='main'>
+				<div>
 				<h1>Ми перші в Україні!</h1>
 				<p>Швидко та в режимі онлайн підготуємо для Вас позовну заяву в суд для оскарження перевищення швидкості руху.</p>
 				<h3>ЮРИДИЧНА ТОЧНІСТЬ</h3>
@@ -26,10 +26,28 @@ const MainSlider = () =>{
 						Дізнатись більше
 					</p>
 				</div>
-				
+				</div>
 			</div>
-		</div>
-			<img src={process.env.PUBLIC_URL + 'img/image1.png'} alt="slider"/>
+			<div className='steps'>
+				<div className='circles'>
+					<div>
+						<p>1</p>
+					</div>
+					<span></span>
+					<div>
+						<p>2</p>
+					</div>
+					<span></span>
+					<div>
+						<p>3</p>
+					</div>
+				</div>
+				<div>
+					<p>Вводите дані, вказані в протоколі порушення</p>
+					<p>Оплачуєте наші послуги,які коштують як горнятко кави</p>
+					<p>Отримуєте ГОТОВУ позовну заяву</p>
+				</div>
+			</div>
 		</div>
 	)
 }
