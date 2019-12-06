849bd5f91d3a356322cf02d0214487fafccf8425
Vladislav Stepaniuk <p.s.vlad2014@gmail.com>
2019-12-06 03:00:51 +0200

    Updates

---
+++ precache-manifest.7f82063efeddde0b34d093e803a338e3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25857832712a1d3f8c5d94004fc7819c",
+    "url": "/project3/index.html"
+  },
+  {
+    "revision": "57802edfd160c6e7b5a0",
+    "url": "/project3/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3fd56ed0550b2c2be2e0",
+    "url": "/project3/static/css/main.0b33cd1b.chunk.css"
+  },
+  {
+    "revision": "57802edfd160c6e7b5a0",
+    "url": "/project3/static/js/2.897b6288.chunk.js"
+  },
+  {
+    "revision": "3fd56ed0550b2c2be2e0",
+    "url": "/project3/static/js/main.c8fe602b.chunk.js"
+  },
+  {
+    "revision": "b5fbf0a3e82c47f1fa4d",
+    "url": "/project3/static/js/runtime-main.5f128550.js"
+  },
+  {
+    "revision": "69c703b21776155b4bf13adbbb72e5a5",
+    "url": "/project3/static/media/Montserrat-Bold.69c703b2.ttf"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "/project3/static/media/Montserrat-Bold.9c71d42b.otf"
+  },
+  {
+    "revision": "d4a8834fa8f57f0929b9f4ef89584361",
+    "url": "/project3/static/media/Montserrat-Light.d4a8834f.otf"
+  },
+  {
+    "revision": "f8fe32d3f0e1dca05f9eece295173bd5",
+    "url": "/project3/static/media/Montserrat-Light.f8fe32d3.ttf"
+  },
+  {
+    "revision": "d815b0a29adf3450c55f56e2fb813be4",
+    "url": "/project3/static/media/Montserrat-Medium.d815b0a2.otf"
+  },
+  {
+    "revision": "f7c2cad2a5287ca6894cc1eb0080d855",
+    "url": "/project3/static/media/Montserrat-Medium.f7c2cad2.ttf"
+  },
+  {
+    "revision": "5906fb82e31864c9b531898bb3c97d98",
+    "url": "/project3/static/media/Montserrat-Regular.5906fb82.ttf"
+  },
+  {
+    "revision": "92db9a0772b3732e6d686fec3711af42",
+    "url": "/project3/static/media/Montserrat-Regular.92db9a07.otf"
+  },
+  {
+    "revision": "b80caec20382c61f0350f74ed67b7b0e",
+    "url": "/project3/static/media/Montserrat-SemiBold.b80caec2.ttf"
+  },
+  {
+    "revision": "bb3740d350b0186ce32b5678972bf061",
+    "url": "/project3/static/media/Montserrat-SemiBold.bb3740d3.otf"
+  },
+  {
+    "revision": "5533d12b4e004db8ffb4a1f826c478f1",
+    "url": "/project3/static/media/image1.5533d12b.png"
+  }
+]);
